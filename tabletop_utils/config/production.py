fecb5e44fa0c560193c552c861e66e7731f389e3
Ty Skelton <tyskelton118@gmail.com>
2020-08-15 11:25:21 -0700

    apparently fargate instances don't allow indexing to a key in a secret

--- tabletop_utils/config/production.py
+++ tabletop_utils/config/production.py
@@ -2,9 +2,9 @@ import os
 
 SECRET_KEY = "TODO2"
 
-sql_host = os.environ.get("SQL_HOST")
-sql_pass = os.environ.get("SQL_PASS")
+sql_info = os.environ.get("SQL_INFO")
+sql_info_dict = json.loads(sql_info)
 
-SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://ttdev:{sql_pass}@{sql_host}/tabletop_utils'
+SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://ttdev:{sql_info_dict['password']}@{sql_info_dict['host']}/tabletop_utils'
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = False
