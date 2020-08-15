16cd8bd45f9fbe423d0b56d929aebaa8f33fb276
Ty Skelton <tyskelton118@gmail.com>
2020-08-15 11:42:52 -0700

    fixes missing dependency and bug in string formatting

--- tabletop_utils/config/production.py
+++ tabletop_utils/config/production.py
@@ -1,10 +1,14 @@
 import os
+import json
 
 SECRET_KEY = "TODO2"
 
 sql_info = os.environ.get("SQL_INFO")
 sql_info_dict = json.loads(sql_info)
 
-SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://ttdev:{sql_info_dict['password']}@{sql_info_dict['host']}/tabletop_utils'
+sql_pass = sql_info_dict['password']
+sql_host = sql_info_dict['host']
+
+SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://admin:{sql_pass}@{sql_host}/tabletop_utils'
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = False
